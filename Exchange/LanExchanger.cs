e370253fc12e2f609461b1ba056e3e3ac0f315e2
Shamrik Dmitriy <shamrik.dmitriy@gmail.com>
2019-07-03 21:01:52 +0600

    Update Lan exchanger code in parts of request and response version 1

--- Exchange/LanExchanger.cs
+++ Exchange/LanExchanger.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,31 +12,142 @@ namespace DevicesControlLibrary.Exchange
 {
     public class LanExchanger
     {
-        public LanExchanger(string ipAddress, int ipPort) {
-            if (string.IsNullOrEmpty(ipAddress) || string.IsNullOrWhiteSpace(ipAddress))
+        #region Private Properties
+
+        /// <summary>
+        ///     Ip address device
+        /// </summary>
+        private IPAddress IpAddress { get; set; }
+
+        /// <summary>
+        ///     Ip port device
+        /// </summary>
+        private int IpPort { get; set; }
+
+        /// <summary>
+        ///     Socket for exchange with device
+        /// </summary>
+        private Socket Socket { get; set; }
+
+        #endregion
+
+        public LanExchanger(string ipAddress, int ipPort)
+        {
+            IpAddress = IPAddress.Parse(ipAddress);
+            IpPort = ipPort; // 1 -65 535
+        }
+
+        /// <summary>
+        ///     Sending a command to the device and returning the result as an integer value
+        /// </summary>
+        /// <param name="command">String, contains command</param>
+        /// <returns>Integer value of response</returns>
+        public int SendWithRequestInt(string command)
+        {
+            return Convert.ToInt32(QueryControl(command));
+        }
+
+        /// <summary>
+        ///     Sending a command to the device and returning the result as an string value
+        /// </summary>
+        /// <param name="command">String, contains command</param>
+        /// <returns>String value of response</returns>
+        public string SendWithRequestString(string command)
+        {
+            return QueryControl(command);
+        }
+
+        /// <summary>
+        ///     Sending a command to the device without returning
+        /// </summary>
+        /// <param name="command">String, contains command</param>
+        public void SendWithoutRequest(string command)
+        {
+            QueryControl(command);
+        }
+
+        /// <summary>
+        ///     Sending a command to the device and returning the result as an double value
+        /// </summary>
+        /// <param name="command">String, contains command</param>
+        /// <returns>Double value of response</returns>
+        public double SendWithRequestDouble(string command)
+        {
+            var currentDoubleSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            var result = QueryControl(command);
+
+            return Convert.ToDouble(!result.Contains(currentDoubleSeparator)
+                ? result.Replace(result.Contains(".") ? "." : ",", currentDoubleSeparator)
+                : QueryControl(command));
+        }
+
+        public void InitConnection()
+        {
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public string QueryControl(string command)
+        {
+            try
             {
-                throw new Exception("Ip-адрес не должен быть пустым");
+                RequestSending(command);
+                return command.IndexOf("?", StringComparison.CurrentCulture) >= 0 ? ResponseReceive() : "";
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
             }
         }
 
-        public int SendWithRequestInt(string command)
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        public bool RequestSending(string command)
         {
-            return 0;
+            var ping = new Ping();
+            var pingReply = ping.Send(IpAddress, 5000);
+
+            if (pingReply != null && pingReply.Status != IPStatus.Success)
+            {
+                throw new Exception("Device not responding");
+            }
+            else
+            {
+                Socket.Send(Encoding.ASCII.GetBytes(command + Environment.NewLine));
+            }
+
+            return true;
         }
 
-        public string SendWithRequestString(string command)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string ResponseReceive()
         {
-            return "";
-        }
+            var buffer = new byte[256];
+            buffer = new byte[buffer.Length];
 
-        public void SendWithoutRequest(string command)
-        {
+            try
+            {
+                int readBytes;
+                do
+                {
+                    readBytes = Socket.Receive(buffer);
+                } while (Socket.Available < 0);
 
-        }
-
-        public double SendWithRequestDouble(string command)
-        {
-            return 0.0;
+                return Encoding.ASCII.GetString(buffer, 0, readBytes);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
         }
     }
 }
